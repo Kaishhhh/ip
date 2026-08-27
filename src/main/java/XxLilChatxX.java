1e19ff3b75844e845d0039e8c2098da01e36f602
Kai Sheng <tkaisheng1@gmail.com>
2026-08-27 23:48:05 +0800

    Added support for list

--- src/main/java/XxLilChatxX.java
+++ src/main/java/XxLilChatxX.java
@@ -15,15 +15,28 @@ public class XxLilChatxX {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
+
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + input);
+                System.out.println("____________________________________________________________");
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________");
         }
 
         System.out.println("Bye. Hope to see you again soon!");
