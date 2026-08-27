75bf5c5bc719e5ec5dc20a829fd6f53b8dc41c2f
Kai Sheng <tkaisheng1@gmail.com>
2026-08-27 23:40:57 +0800

    Added support for echo

--- src/main/java/XxLilChatxX.java
+++ src/main/java/XxLilChatxX.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class XxLilChatxX {
     public static void main(String[] args) {
         String banner =
@@ -12,6 +14,17 @@ public class XxLilChatxX {
         System.out.println("Hello! I'm XxLilChatxX.");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        }
 
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
